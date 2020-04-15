106d76e7d477cae8a4cea46fbaeea0ffa1cb12ec
Kayla San Filippo <Kaylasf@Kaylas-MacBook-Air.local>
2020-04-14 18:19:13 -0700

    config

--- models/index.js
+++ models/index.js
@@ -8,9 +8,7 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if(process.env.JAWSDB_URL){
-  connection = Sequelize.createConnection(process.env.JAWSDB_URL);
-}
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
